ed6a52a110d943078915f5e428a88d1f95f1b729
Nikolai Emil Damm <neq@energinet.dk>
2025-02-16 23:58:31 +0100

    feat: add EncryptAsync and DecryptAsync methods to SOPSLocalAgeSecretManager
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

---
+++ Devantler.SecretManager.SOPS.LocalAge.Tests/SOPSLocalAgeSecretManagerTests/DecryptAsyncTests.cs
@@ -0,0 +1,39 @@
+namespace Devantler.SecretManager.SOPS.LocalAge.Tests.SOPSLocalAgeSecretManagerTests;
+
+/// <summary>
+/// Tests for <see cref="SOPSLocalAgeSecretManager.DecryptAsync(string, CancellationToken)"/>.
+/// </summary>
+[Collection("SOPSLocalAgeSecretManager")]
+public class DecryptAsyncTests
+{
+  readonly SOPSLocalAgeSecretManager _secretManager = new();
+
+  /// <summary>
+  /// Tests that <see cref="SOPSLocalAgeSecretManager.DecryptAsync(string, CancellationToken)"/> decrypts a file with a private key.
+  /// </summary>
+  /// <returns></returns>
+  [Fact]
+  public async Task DecryptAsync_GivenPathAndPublicKey_ReturnsDecryptedString()
+  {
+    // Arrange
+    var key = await _secretManager.CreateKeyAsync();
+    string plainText = "secret: AGE-SECRET-KEY-1VZQ";
+    string filePath = Path.GetTempPath() + "decrypt-async-test.yaml";
+    File.WriteAllText(filePath, plainText);
+
+    // Act
+    string encryptedText = await _secretManager.EncryptAsync(filePath, key.PublicKey);
+    File.WriteAllText(filePath, encryptedText);
+    string decryptedText = await _secretManager.DecryptAsync(filePath);
+
+    // Assert
+    Assert.NotEqual(plainText, encryptedText);
+    Assert.NotEqual(encryptedText, decryptedText);
+    // ignore whitespace and newlines
+    Assert.Equal(plainText.TrimEnd(), decryptedText.TrimEnd());
+
+    // Cleanup
+    _ = await _secretManager.DeleteKeyAsync(key);
+  }
+}
+
---
+++ Devantler.SecretManager.SOPS.LocalAge.Tests/SOPSLocalAgeSecretManagerTests/EncryptAsyncTests.cs
@@ -0,0 +1,37 @@
+namespace Devantler.SecretManager.SOPS.LocalAge.Tests.SOPSLocalAgeSecretManagerTests;
+
+/// <summary>
+/// Tests for <see cref="SOPSLocalAgeSecretManager.EncryptAsync(string, string, CancellationToken)"/>.
+/// </summary>
+[Collection("SOPSLocalAgeSecretManager")]
+public class EncryptAsyncTests
+{
+  readonly SOPSLocalAgeSecretManager _secretManager = new();
+
+  /// <summary>
+  /// Tests that <see cref="SOPSLocalAgeSecretManager.EncryptAsync(string, string, CancellationToken)"/> encrypts a file with a public key.
+  /// </summary>
+  /// <returns></returns>
+  [Fact]
+  public async Task EncryptAsync_GivenPathAndPublicKey_ReturnsEncryptedString()
+  {
+    // Arrange
+    var key = await _secretManager.CreateKeyAsync();
+    string plainText = """
+      secret: |
+        AGE-SECRET-KEY-1VZQ
+    """;
+    string filePath = Path.GetTempPath() + "encrypt-async-test.yaml";
+    File.WriteAllText(filePath, plainText);
+
+    // Act
+    string encryptedText = await _secretManager.EncryptAsync(filePath, key.PublicKey);
+
+    // Assert
+    Assert.NotEqual(plainText, encryptedText);
+
+    // Cleanup
+    _ = await _secretManager.DeleteKeyAsync(key);
+  }
+}
+
