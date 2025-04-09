fe20eda71ce0509f7f6d68d2babe54eac20093cc
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-04-09 14:53:25 +0200

    test: update tests
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- tests/Devantler.SecretManager.SOPS.LocalAge.Tests/SOPSLocalAgeSecretManagerTests/DeleteKeyAsyncTests.cs
+++ tests/Devantler.SecretManager.SOPS.LocalAge.Tests/SOPSLocalAgeSecretManagerTests/DeleteKeyAsyncTests.cs
@@ -15,13 +15,31 @@ public class DeleteKeyAsyncTests
   /// </summary>
   /// <returns></returns>
   [Fact]
-  public async Task DeleteKeyAsync_DeletesKeyFromSOPSKeyFile()
+  public async Task DeleteKeyAsyncWithAgeKey_DeletesKeyFromSOPSKeyFile()
   {
     // Arrange
     var key = await _secretManager.CreateKeyAsync();
 
     // Act
     _ = await _secretManager.DeleteKeyAsync(key);
+    bool keyExists = await _secretManager.KeyExistsAsync(key.PublicKey);
+
+    // Assert
+    Assert.False(keyExists);
+  }
+
+  /// <summary>
+  /// Tests that <see cref="SOPSLocalAgeSecretManager.DeleteKeyAsync(string, CancellationToken)"/> deletes a key from the SOPS key file when no key path is provided.
+  /// </summary>
+  /// <returns></returns>
+  [Fact]
+  public async Task DeleteKeyAsyncWithPublicKeyString_DeletesKeyFromSOPSKeyFile()
+  {
+    // Arrange
+    var key = await _secretManager.CreateKeyAsync();
+
+    // Act
+    _ = await _secretManager.DeleteKeyAsync(key.PublicKey);
     bool keyExists = await _secretManager.KeyExistsAsync(key.PublicKey);
 
     // Assert
